9d7a5d436921e137f399502917f2b19df75f6e9d
Horst Schneider <horst.schneider@posteo.de>
2015-12-24 16:54:03 +0100

    managed to show a window, somehow. still no idea how to use bitflags in rust, and we need refactoring.

--- src/awm.rs
+++ src/awm.rs
@@ -2,10 +2,49 @@ extern crate libc;
 extern crate xlib;
 extern crate xinerama;
 
+/*
+use xlib::XCreateSimpleWindow;
+use xlib::XBlackPixel;
+use xlib::XWhitePixel;
+use xlib::XMapWindow;
+use xlib::XDefaultScreen;
+use xlib::XEvent;
+use xlib::XNextEvent;
+use xlib::XSelectInput;
+use xlib::XInputEventMasks;
+*/
+use xlib::*;
 use window_system::WindowSystem;
+
 
 mod window_system;
 
 fn main() {
 	println!("Hallo, Welt!");
+
+    let window_system = WindowSystem::new();
+    unsafe {
+        let border = XBlackPixel(window_system.display, XDefaultScreen(window_system.display));
+        let background = XWhitePixel(window_system.display, XDefaultScreen(window_system.display));
+        let x = 50;
+        let y = 50;
+        let width = 100;
+        let height = 100;
+        let border_width = 10;
+        let window = XCreateSimpleWindow(window_system.display, window_system.root, x,y, width, height,border_width, border,background);
+        //XSelectInput(window_system.display, window, ExposureMask | KeyPressMask );
+        XSelectInput(window_system.display, window, (1 << 0) | (1 << 15));
+        XMapWindow(window_system.display, window);
+    }
+
+    let mut e = XEvent { _type : 0, pad: [0;24] };
+    
+    while true {
+        unsafe {
+            XNextEvent(window_system.display, &mut e);
+            if(e._type == Expose) {println!("test");}
+            if(e._type == KeyPress) {break;}
+        }
+
+    }
 }
--- src/window_system.rs
+++ src/window_system.rs
@@ -1,8 +1,23 @@
 use std::ptr;
-use xlib::{ Display, Window };
+use xlib::{ Display, Window, Screen };
 use xlib::{ XOpenDisplay, XDefaultScreenOfDisplay, XRootWindowOfScreen };
 
 pub struct WindowSystem {
-    display:  *mut Display,
-    root: Window
+    pub display:  *mut Display,
+    pub root: Window,
+}
+
+impl WindowSystem {
+   pub fn new() -> WindowSystem {
+        unsafe {
+            let display = XOpenDisplay(ptr::null_mut()); // Null Pointer means "just use env variable $DISPLAY
+            let screen = XDefaultScreenOfDisplay(display);
+            let root = XRootWindowOfScreen(screen);
+
+            WindowSystem {
+                display: display,
+                root: root,
+            }
+        }
+    }
 }
