09369e48f51ffdb1c7dfbe77e0141e37df7727af
Horst Schneider <h.schneider@hs-mannheim.de>
2016-01-13 15:11:13 +0100

    now drawing real frame window

--- src/awm.rs
+++ src/awm.rs
@@ -14,27 +14,9 @@ fn main() {
     let window_system = WindowSystem::new();
 
     unsafe {
-        let border = XWhitePixel(window_system.display, XDefaultScreen(window_system.display));
-        let background = XBlackPixel(window_system.display, XDefaultScreen(window_system.display));
-        let x = 50;
-        let y = 50;
-        let width = 100;
-        let height = 100;
-        let border_width = 10;
-        let window = XCreateSimpleWindow(window_system.display, 
-                                         window_system.root, 
-                                         x, 
-                                         y, 
-                                         width, 
-                                         height,
-                                         border_width, 
-                                         border,background);
-
         XSelectInput(window_system.display, 
                      window_system.root, 
                          SubstructureNotifyMask | SubstructureRedirectMask);
-        
-        XMapWindow(window_system.display, window);
     }
 
     let mut event = XEvent { pad: [0;24] };
--- src/handlers.rs
+++ src/handlers.rs
@@ -1,8 +1,36 @@
 use x11::xlib;
 use window_system::WindowSystem;
+use libc::c_ulong;
 
 pub struct KeyPressedHandler; 
 pub struct MapRequestHandler;
+
+fn create_some_window(window_system: &WindowSystem) -> c_ulong {
+    let x = 0;
+    let y = 0;
+    let width = 500;
+    let height = 300;
+    let border_width=5;
+    unsafe {
+        let border = xlib::XWhitePixel(window_system.display, 
+                                       xlib::XDefaultScreen(window_system.display));
+        let background = xlib::XBlackPixel(window_system.display, 
+                                           xlib::XDefaultScreen(window_system.display));
+        let window = xlib::XCreateSimpleWindow(window_system.display, 
+                                     window_system.root, 
+                                     x, 
+                                     y, 
+                                     width, 
+                                     height,
+                                     border_width, 
+                                     border,background);
+        xlib::XSelectInput(window_system.display, 
+                           window, 
+                           xlib::SubstructureNotifyMask | xlib::SubstructureRedirectMask);
+
+        return window;
+    }
+}
 
 impl MapRequestHandler {
     pub fn new() -> MapRequestHandler {
@@ -11,9 +39,13 @@ impl MapRequestHandler {
 
     pub fn handle(&self, event: xlib::XEvent, window_system: &WindowSystem) {
         let event = xlib::XMapRequestEvent::from(event);
-        
+       
+        let frame = create_some_window(window_system);
         unsafe {
-            xlib::XMapWindow(window_system.display, event.window); 
+            xlib::XResizeWindow(window_system.display, event.window, 500,300);
+            xlib::XReparentWindow(window_system.display, event.window, frame, 0, 0);
+            xlib::XMapWindow(window_system.display, frame); 
+            xlib::XMapWindow(window_system.display, event.window);
         }
 
         println!("Handling MapRequest");
